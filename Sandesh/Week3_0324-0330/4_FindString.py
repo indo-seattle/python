b43513716b14005a487fa6c73f3ba590c7beaaec
sandeshedara <sandeshpec@gmail.com>
2019-03-27 20:41:21 -0700

    Delete 4_FindString.py

--- Sandesh/Week3_0324-0330/4_FindString.py
+++
@@ -1,7 +0,0 @@
-string = "I'm living in Washington"
-y = input("Please enter the string you want to find : ")
-x = string.find(y)
-if x == -1:
-    print ("Sorry, couldn't find the string")
-else:
-    print ("I found the string you're looking for which is character", x)

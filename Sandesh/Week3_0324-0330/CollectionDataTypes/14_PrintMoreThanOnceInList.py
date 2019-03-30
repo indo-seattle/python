a8042e93da7a0af7c0be3f21a0ecbd3040dc55d0
sandeshedara <sandeshpec@gmail.com>
2019-03-30 02:30:54 -0700

    Delete 14_PrintMoreThanOnceInList.py

--- Sandesh/Week3_0324-0330/CollectionDataTypes/14_PrintMoreThanOnceInList.py
+++
@@ -1,6 +0,0 @@
-#Write a Python program that takes mylist = [“CAL”, “SEA”, “PIT”, “CAL”] and print the string that appeared more than once.
-mylist = ["CAL", "SEA", "PIT", "CAL1"]
-mylist.sort()
-for x in mylist:
-    if x == mylist[1]:
-        print(x)

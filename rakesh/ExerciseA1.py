08feeb2c5590272243e3901d5d03e2bc0e801bba
rbg14952 <rakesh_gujj@yahoo.com>
2019-04-04 07:18:40 -0700

    Exercises (Data types and Loops)

---
+++ rakesh/ExerciseA1.py
@@ -0,0 +1,8 @@
+print("Exercise 1 - Python Dictionary & using python modules")
+mydict = {'bellevue': 'Microsoft', 'Seattle': 'Amazon', 'Everett':'Boeing'}
+
+for mycity in mydict:
+    # print('Key :', mycity)
+    # print('Value :', mydict[mycity])
+
+    print ( mydict[mycity].capitalize(), ' started in ', mycity.capitalize())

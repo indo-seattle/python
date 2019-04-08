d5eb99e06a3b65653171f1ac574b00ba1f1e9886
Sayaram Gattu <sayaram@Sayarams-MacBook-Pro.local>
2019-04-08 00:20:08 -0700

    Homework Book Store

--- Sayaram/ManageBook.py
+++ Sayaram/ManageBook.py
@@ -3,4 +3,8 @@ import BookStore
 b = BookStore.Book()
 b.addBook("Bible", 123, 60)
 b.addBook("Harry Poter", 124, 50)
+b.addBook("Harry Poter", 124, 50)
 print(b.bookList)
+b.delBook("Harry Poter")
+print(b.bookList)
+

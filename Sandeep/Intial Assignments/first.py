bd138a04a7f2cfecb0ed012a4e2e0f0cd0c08e9e
Sandeep <sandyindpdt@gmail.com>
2019-04-14 18:55:51 -0700

    Reorganized the folders

---
+++ Sandeep/Intial Assignments/first.py
@@ -0,0 +1,19 @@
+#print('Sandeep')
+
+#hello=input(print("Enter your name"))
+#print("Hello "+hello)
+
+list =[1,2,3,4,5]
+list1=[6,7,8]
+
+list =list+list1
+
+list.insert(0,0)
+
+#list=[]
+#list.pop(-1)
+#for i in list:
+ #   print(i)
+
+
+

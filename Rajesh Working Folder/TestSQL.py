511d46ca841735f9b1e4c3f7535ddbdc3473191f
rgodavarthi <rgodavarthi@users.noreply.github.com>
2019-05-18 16:48:17 -0700

    MySQL basics

---
+++ Rajesh Working Folder/TestSQL.py
@@ -0,0 +1,56 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="1234",
+  database = "mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+# mycursor.execute("SELECT * FROM customers")
+
+mycursor.execute("SELECT * FROM states_capitals")
+
+#
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x[0], x[1])
+
+# sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+# val = ("John", "Highway 21")
+
+# sql = "INSERT INTO states_capitals (state, capital) VALUES (%s, %s)"
+# val = [
+#     ('Washington', 'Olympia'),
+#     ('Oregon', 'Portland'),
+#     ('California', 'Sacremento')
+#     ]
+#
+# mycursor.executemany(sql, val)
+#
+# mydb.commit()
+
+# print(mycursor.rowcount, "record inserted.")
+
+# mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+
+# mycursor.execute("CREATE TABLE states_capitals (state VARCHAR(255), capital VARCHAR(255))")
+#
+# mycursor.execute("SHOW TABLES")
+#
+# for x in mycursor:
+#   print(x)
+
+
+# mycursor.execute("SHOW DATABASES")
+#
+# for x in mycursor:
+#   print(x)
+
+# mycursor.execute("CREATE DATABASE mydatabase")
+
+
+print(mydb)
